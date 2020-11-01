b6f499c78447126430f8a7a9198be4989447c096
tfull <tfurukawa.mail@gmail.com>
2020-11-02 00:04:22 +0900

    board

--- commands/board.py
+++ commands/board.py
@@ -1,4 +1,5 @@
 import tkinter
+from PIL import Image, ImageDraw
 
 import _pre
 
@@ -8,50 +9,85 @@ class Board:
         self.window = tkinter.Tk()
         self.window.title("かな入力")
 
-        self.canvas = tkinter.Canvas(self.window, bg = "black", width = 512, height = 256)
-        self.canvas.pack()
+        self.frame = tkinter.Frame(self.window, width = 1300, height = 300)
+        self.frame.pack()
+
+        self.canvas = tkinter.Canvas(self.frame, bg = "black", width = 1280, height = 256)
+        self.canvas.place(x = 0, y = 0)
 
         self.canvas.bind("<ButtonPress-1>", self.click_left)
         self.canvas.bind("<B1-Motion>", self.drag_left)
         self.canvas.bind("<ButtonPress-3>", self.click_right)
         self.canvas.bind("<B3-Motion>", self.drag_right)
 
+        self.button = tkinter.Button(self.frame, bg = "blue", fg = "yellow", text = "認識", width = 100, height = 40, command = self.press_button)
+        self.button.place(x = 0, y = 256)
+
+        self.label = tkinter.Label(self.frame, bg = "green", fg = "yellow", text = "hello", width = 400, height = 40)
+        self.label.place(x = 100, y = 256)
+
+        self.image = Image.new("L", (1280, 256))
+        self.draw = ImageDraw.Draw(self.image)
+
+    def press_button(self):
+        self.image.save("tmp.png")
+
     def click_left(self, event):
+        ex = event.x
+        ey = event.y
+
         self.canvas.create_oval(
-            event.x, event.y, event.x, event.y,
+            ex, ey, ex, ey,
             outline = "white",
             width = 8
         )
 
-        self.x = event.x
-        self.y = event.y
+        self.draw.ellipse((ex - 4, ey - 4, ex + 4, ey + 4), fill = 255)
+
+        self.x = ex
+        self.y = ey
 
     def drag_left(self, event):
+        ex = event.x
+        ey = event.y
+
         self.canvas.create_line(
-            self.x, self.y, event.x, event.y,
+            self.x, self.y, ex, ey,
             fill = "white",
             width = 8
         )
 
-        self.x = event.x
-        self.y = event.y
+        self.draw.line((self.x, self.y, ex, ey), fill = 255, width = 8)
+
+        self.x = ex
+        self.y = ey
 
     def click_right(self, event):
+        ex = event.x
+        ey = event.y
+
         self.canvas.create_oval(
-            event.x, event.y, event.x, event.y,
+            ex, ey, ex, ey,
             outline = "black",
             width = 8
         )
+
+        self.draw.ellipse((ex - 4, ey - 4, ex + 4, ey + 4), fill = 0)
 
         self.x = event.x
         self.y = event.y
 
     def drag_right(self, event):
+        ex = event.x
+        ey = event.y
+
         self.canvas.create_line(
-            self.x, self.y, event.x, event.y,
+            self.x, self.y, ex, ey,
             fill = "black",
             width = 8
         )
+
+        self.draw.line((self.x, self.y, ex, ey), fill = 0, width = 8)
 
         self.x = event.x
         self.y = event.y
